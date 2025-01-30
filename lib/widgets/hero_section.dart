0a8c40094825f491c6f4945c6ade44b6b82c736b
tushar-ventura <48640828+trick4u@users.noreply.github.com>
2025-01-30 17:36:57 +0530

    animatedBackground

--- lib/widgets/hero_section.dart
+++ lib/widgets/hero_section.dart
@@ -1,3 +1,6 @@
+import 'dart:async';
+
+import 'package:animated_background/animated_background.dart';
 import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -8,7 +11,41 @@ import 'dart:math' as math;
 import 'left_side.dart';
 import 'middle_hero.dart';
 
-class HeroSection extends StatelessWidget {
+class HeroSection extends StatefulWidget {
+  @override
+  State<HeroSection> createState() => _HeroSectionState();
+}
+
+class _HeroSectionState extends State<HeroSection>
+    with TickerProviderStateMixin {
+  late Timer _colorTimer;
+  int _colorIndex = 0;
+  final List<Color> _colors = [
+    Colors.blue,
+    Colors.purple,
+    Colors.teal,
+    Colors.red,
+    Colors.orange,
+    Colors.green,
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+    // Change color every 5 seconds
+    _colorTimer = Timer.periodic(const Duration(seconds: 5), (timer) {
+      setState(() {
+        _colorIndex = (_colorIndex + 1) % _colors.length;
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    _colorTimer.cancel();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     final bool isMobileLayout = MediaQuery.of(context).size.width < 600;
@@ -18,11 +55,27 @@ class HeroSection extends StatelessWidget {
     return Container(
       height: screenHeight,
       width: screenWidth,
-      child: isMobileLayout
-          ? _buildMobileLayout(context)
-          : _buildDesktopLayout(context),
+      child: AnimatedBackground(
+        behaviour: RandomParticleBehaviour(options: particles),
+        vsync: this,
+        child: isMobileLayout
+            ? _buildMobileLayout(context)
+            : _buildDesktopLayout(context),
+      ),
     );
   }
+  ParticleOptions get particles => ParticleOptions(
+        baseColor: _colors[_colorIndex],
+        spawnOpacity: 0.0,
+        opacityChangeRate: 0.25,
+        minOpacity: 0.1,
+        maxOpacity: 0.4,
+        particleCount: 70,
+        spawnMaxRadius: 15.0,
+        spawnMaxSpeed: 100.0,
+        spawnMinSpeed: 30,
+        spawnMinRadius: 7.0,
+      );
 
   Widget _buildDesktopLayout(BuildContext context) {
     return Row(
